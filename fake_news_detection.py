889cb39b9cb444111aa656b31f20a83c676628c5
Tejas Dahad <56474128+tejasdahad@users.noreply.github.com>
2021-04-08 10:39:44 +0530

    second commit

--- fake_news_detection.py
+++ fake_news_detection.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 import pandas as pd
 import itertools
@@ -17,7 +16,6 @@ labels.head()
 x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
 
 tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
-#DataFlair - Fit and transform train set, transform test set
 tfidf_train=tfidf_vectorizer.fit_transform(x_train) 
 tfidf_test=tfidf_vectorizer.transform(x_test)
 
@@ -25,7 +23,6 @@ tfidf_test=tfidf_vectorizer.transform(x_test)
 pac=PassiveAggressiveClassifier(max_iter=50)
 pac.fit(tfidf_train,y_train)
 
-#DataFlair - Predict on the test set and calculate accuracy
 y_pred=pac.predict(tfidf_test)
 score=accuracy_score(y_test,y_pred)
 print(f'Accuracy: {round(score*100,2)}%')
